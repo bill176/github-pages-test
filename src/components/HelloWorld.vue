35f2659dde0e6c51401b23772d0b1f4909a84b12
Bill Zhang <bill.nonbusiness@gmail.com>
2023-12-31 12:37:23 -0500

    added test for fetching

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+
+type Post = {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+
 defineProps<{
   msg: string
 }>()
+
+const emptyPost: Post = { userId:0, id:0, title:'', body:'' }
+
+let body = ref<Post>(emptyPost)
+
+onMounted(async () => {
+  await new Promise(r => setTimeout(r, 2000));
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  body.value = await response.json()
+})
 </script>
 
 <template>
@@ -12,6 +32,11 @@ defineProps<{
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <div v-if="body.userId !== 0">
+      <h3>Here is a post for you:</h3>
+      <h3>{{ body.title }}</h3>
+      <h3>{{ body.body }}</h3>
+    </div>
   </div>
 </template>
 
